925c3bf5a2318cb049f583e6aac4ddf175fe18a6
sklbancor <109073706+sklbancor@users.noreply.github.com>
2023-02-02 22:33:24 +0000

    VERSION 1.2.1

---
+++ Frozen/Demo7-4/version.py
@@ -0,0 +1,12 @@
+"""
+Demo7-4 -- VERSION NUMBER
+"""
+__VERSION__ = "1.2.1"
+__DATE__ = "02/02/2023"
+
+def print_version():
+    """
+    prints the version of this simulation package
+    """
+    print(f"Carbon Sim v{__VERSION__} ({__DATE__})")
+    
